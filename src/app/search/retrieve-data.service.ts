bbb3e945fcab89a72764409b5bef2d5f27716c0f
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2017-11-23 15:43:15 +0100

    Send accept and content-type headers along with search request

--- src/app/search/retrieve-data.service.ts
+++ src/app/search/retrieve-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { MapResultToModelService } from '../models/map-result-to-model.service';
 
@@ -9,9 +9,11 @@ export class RetrieveDataService {
   constructor(private http: HttpClient, private mrtm: MapResultToModelService) { }
 
   getData(searchTerm: string) {
+    const httpHeaders = new HttpHeaders({'Accept': 'application/json'});
+    httpHeaders.append('Content-Type', 'application/json');
     return this.http
-      .get('https://rcs.philsem.unibas.ch/json/' + searchTerm)
       .map(res => this.mrtm.parseResult(res));
+      .get('https://rcs.philsem.unibas.ch/json/' + searchTerm, {headers: httpHeaders});
   }
 
 }
