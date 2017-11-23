8e816fd1d452f2475c5ada6dce09feedc0f9f7d4
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2017-11-23 15:46:00 +0100

    Parse result object in searchfield.component instead of retrieve-data.service

--- src/app/search/retrieve-data.service.ts
+++ src/app/search/retrieve-data.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import { MapResultToModelService } from '../models/map-result-to-model.service';
 
 @Injectable()
 export class RetrieveDataService {
 
-  constructor(private http: HttpClient, private mrtm: MapResultToModelService) { }
+  constructor(private http: HttpClient) { }
 
   getData(searchTerm: string) {
     const httpHeaders = new HttpHeaders({'Accept': 'application/json'});
     httpHeaders.append('Content-Type', 'application/json');
     return this.http
-      .map(res => this.mrtm.parseResult(res));
       .get('https://rcs.philsem.unibas.ch/json/' + searchTerm, {headers: httpHeaders});
   }
 
--- src/app/search/searchfield/searchfield.component.ts
+++ src/app/search/searchfield/searchfield.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RetrieveDataService } from '../retrieve-data.service';
 import { FormControl } from '@angular/forms';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/switchMap';
-import { Commentator } from '../../models/commentator';
+import { MapResultToModelService } from '../../models/map-result-to-model.service';
+import { ResultStreamerService } from '../result-streamer.service';
 
 
 @Component({
@@ -14,26 +15,22 @@ import { Commentator } from '../../models/commentator';
 export class SearchfieldComponent implements OnInit {
 
   searchInput: FormControl = new FormControl('');
-  @Output() resultArray: EventEmitter<Commentator[]> = new EventEmitter<Commentator[]>();
 
-  constructor(private retrieveData: RetrieveDataService) {
+  constructor(private retrieveData: RetrieveDataService,
+              private mrtm: MapResultToModelService,
+              private rs: ResultStreamerService) {
     this.searchInput
       .valueChanges
       .debounceTime(500)
       .switchMap(searchString => retrieveData.getData(searchString))
       .subscribe(
-        res => {console.log(res); return this.emitResultArray(res);},
-        err => { console.log('Error!')},
+        res => this.rs.pushResultStream(this.mrtm.parseResult(res)),
+        err => console.log(`Can't get results. Error code: %s, URL: %s`, err.message, err.url),
         () => console.log('Results retrieved')
       );
   }
 
   ngOnInit() {
   }
-
-  emitResultArray(result: Commentator[]) {
-    this.resultArray.emit(result);
-  }
-
 
 }
